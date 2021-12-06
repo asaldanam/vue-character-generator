c9c51e87759da793ce1161559f41eb17f6b62e5d
asaldanam <asaldanam@gmail.com>
2021-12-06 19:38:46 +0100

    refactor

---
+++ components/Character/Stats/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="root">
+    <div v-for="stat in stats" :key="stat">
+      <CharacterStatsItem :statId="stat" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, toRefs, computed } from '@nuxtjs/composition-api';
+import { CHARACTER_STATS } from '~/models/character/stats';
+
+export default defineComponent({
+  props: {
+    statsType: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const { statsType } = toRefs(props);
+    const statKeys = Object.keys(CHARACTER_STATS);
+    const stats = computed(() => statKeys?.filter((s) => s.startsWith(statsType.value)));
+    return { stats };
+  },
+});
+</script>
--- components/Character/index.vue
+++ components/Character/index.vue
@@ -1,38 +1,25 @@
 <template>
   <main>
-    <div v-for="attr in attrs" :key="attr">
-      <CharacterStat :statId="attr" editMode />
-    </div>
-    <div v-for="skill in skills" :key="skill">
-      <CharacterStat :statId="skill" editMode />
-    </div>
+    <CharacterStats :statsType="'attr'" />
+    <CharacterStats :statsType="'skill'" />
     <CharacterActions />
   </main>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, useContext, useRoute } from '@nuxtjs/composition-api';
+import { defineComponent, onMounted, useContext } from '@nuxtjs/composition-api';
 import useCharacterSheet from '~/composables/stores/useCharacterStore';
-import { CHARACTER_STATS } from '~/models/character/stats';
 
 export default defineComponent({
   setup() {
-    const [character, { load }] = useCharacterSheet.injectors();
+    const [, { load }] = useCharacterSheet.injectors();
     const { query } = useContext();
-
-    const stats = Object.keys(CHARACTER_STATS);
-    const attrs = stats.filter((stat) => stat.startsWith('attr_'));
-    const skills = stats.filter((stat) => stat.startsWith('skill_'));
 
     onMounted(() => {
       load((query.value as any)?.character);
     });
 
-    return {
-      attrs,
-      skills,
-      character,
-    };
+    return {};
   },
 });
 </script>
