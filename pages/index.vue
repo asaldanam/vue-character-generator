80ae46e6f485073d2dfe85d3cc959d3f8d372a65
asaldanam <asaldanam@gmail.com>
2022-05-04 00:31:11 +0200

    redirect to character

--- pages/index.vue
+++ pages/index.vue
@@ -5,9 +5,15 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api';
+import { defineComponent, onMounted, useRouter } from '@nuxtjs/composition-api';
 
 export default defineComponent({
-  setup() {},
+  setup() {
+    const router = useRouter();
+
+    onMounted(() => {
+      router.push('/character');
+    });
+  },
 });
 </script>
