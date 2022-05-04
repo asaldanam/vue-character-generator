440abd23148034c6ae129419cb08a5533c716da1
asaldanam <asaldanam@gmail.com>
2022-05-04 07:59:09 +0200

    list

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,36 @@
 <template>
-  <div>
-    <a href="/character">/character</a>
-  </div>
+  <ul>
+    <li v-for="character in characters" :key="character">
+      <a :href="`/character/${character}`">{{ character }}</a>
+    </li>
+
+    <li>
+      <a href="/character">Nuevo personaje</a>
+    </li>
+  </ul>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, useRouter } from '@nuxtjs/composition-api';
+import { computed, defineComponent, onMounted, useRouter } from '@nuxtjs/composition-api';
 
 export default defineComponent({
   setup() {
     const router = useRouter();
-
-    onMounted(() => {
-      router.push('/character');
+    const characters = computed(() => {
+      const storageStr = localStorage.getItem('characters') || '{}';
+      const storage = JSON.parse(storageStr);
+      const list = Object.keys(storage);
+      return list;
     });
+
+    return {
+      characters,
+    };
   },
 });
 </script>
+<style lang="scss" scoped>
+a {
+  color: white;
+}
+</style>
