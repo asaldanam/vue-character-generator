6779c72ff44f6f746ed1e4bc778541a5e05f62e0
asaldanam <asaldanam@gmail.com>
2022-12-06 09:58:56 +0100

    audio

--- composables/stores/useDiceRollerStore.ts
+++ composables/stores/useDiceRollerStore.ts
@@ -2,6 +2,15 @@ import { ref } from "@nuxtjs/composition-api";
 import random from 'random';
 import { createStore } from "~/shared/libs/createStore";
 
+const diceRollAudio = new Audio('/audio/roll-dice.mp3')
+diceRollAudio.volume = 0.5;
+
+const failureAudio = new Audio('/audio/roll-failure.wav')
+failureAudio.volume = 0.2;
+
+const successAudio = new Audio('/audio/roll-success.wav')
+successAudio.volume = 0.5;
+
 export default createStore(() => {
   const state = ref({
     isOpen: false,
@@ -17,17 +26,23 @@ export default createStore(() => {
       throwDice(payload: { success: number; successMsg?: string; failureMsg?: string; }) {
         state.value.isOpen = true;
 
+        const randomUniformNumber = random.uniform(0, 1);
+        const randomNumber = randomUniformNumber();
+
+        // solo para D20
+        const roll = Math.ceil(randomNumber * 20);
+        const success = roll >= payload.success;
+
         setTimeout(() => {
-          const randomUniformNumber = random.uniform(0, 1);
-          const randomNumber = randomUniformNumber();
-
-          // solo para D20
-          state.value.roll = Math.ceil(randomNumber * 20);
-
-          state.value.hasSuccess = state.value.roll >= payload.success;
+          state.value.roll = roll;
+          state.value.hasSuccess = success;
           state.value.successMsg = payload.successMsg || '';
           state.value.failureMsg = payload.failureMsg || '';
         })
+
+        setTimeout(() => diceRollAudio.play(), 200)
+        setTimeout(() => success ? successAudio.play() : failureAudio.play(), 1300)
+
       },
       close() {
         state.value.isOpen = false;
